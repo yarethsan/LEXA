a3cb2e5b9c7fbadf114ac1a48ef3c24f989afd7e
yarethsan <joogalaxy00@gmail.com>
2024-11-10 23:07:18 -0600

    Se modifica la ñ

--- src/main/java/com/lexa/punto/_de/_venta/modelos/Usuario.java
+++ src/main/java/com/lexa/punto/_de/_venta/modelos/Usuario.java
@@ -1,5 +1,6 @@
 package com.lexa.punto._de._venta.modelos;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.lexa.punto._de._venta.util.Roles;
 import jakarta.persistence.*;
 import lombok.ToString;
@@ -17,17 +18,19 @@ public class Usuario {
     @Column(name = "id_usuario")
 private Integer idUsuario;
 private String nombre;
-    @Column(name = "contraseña")
+    @Column(name = "contrasena")
 private String contrasena;
 
     @OneToMany(mappedBy ="idUsuario",  fetch = FetchType.LAZY,
             cascade = CascadeType.ALL)
+    @JsonIgnore
     private Set<Venta> venta;
+
 
 @OneToOne()
 @JoinColumn(name = "rol", referencedColumnName = "id_rol")
 private Rol rol;
-
+private boolean activo = true;
     //GET Y SET
 
     public Integer getIdUsuario() {
@@ -68,5 +71,13 @@ private Rol rol;
 
     public void setRol(Rol rol) {
         this.rol = rol;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
     }
 }
