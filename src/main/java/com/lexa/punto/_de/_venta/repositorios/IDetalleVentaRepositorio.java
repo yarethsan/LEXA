16e3a4c40cd978e3e46ecf3e742138ad775a7df1
yarethsan <joogalaxy00@gmail.com>
2024-11-10 23:08:07 -0600

    Se quita el eliminar usuario

--- src/main/java/com/lexa/punto/_de/_venta/repositorios/IDetalleVentaRepositorio.java
+++ src/main/java/com/lexa/punto/_de/_venta/repositorios/IDetalleVentaRepositorio.java
@@ -1,9 +1,12 @@
 package com.lexa.punto._de._venta.repositorios;
 
-import com.lexa.punto._de._venta.modelos.Venta;
+import com.lexa.punto._de._venta.modelos.DetalleVenta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface IDetalleVentaRepositorio extends JpaRepository <Venta, Integer> {
+public interface IDetalleVentaRepositorio extends JpaRepository<DetalleVenta, Integer> {
+    List<DetalleVenta> findByVenta_idVenta(Integer idVenta);
 }
--- src/main/java/com/lexa/punto/_de/_venta/servicios/IUsuarioService.java
+++ src/main/java/com/lexa/punto/_de/_venta/servicios/IUsuarioService.java
@@ -3,9 +3,17 @@ package com.lexa.punto._de._venta.servicios;
 import com.lexa.punto._de._venta.modelos.Usuario;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
+
 public interface IUsuarioService {
 
 
     //METODOS ABSTRACTOS
     Usuario agregarUsuario(Usuario usuario) throws ResponseStatusException;
+
+    //METODOS AGREGADO DE EXPERIMENTO
+    Usuario obtenerUsuarioId(Integer id) throws ResponseStatusException;
+    List<Usuario> obtenerTodosUsuarios();
+    void  pausarUsuario(Integer idUsuario);
+    // void eliminarUsuario (Integer idUsuario);
 }
