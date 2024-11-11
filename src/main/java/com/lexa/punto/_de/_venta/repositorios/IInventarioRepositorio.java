dbb0ef8414a0a838c3ef3aa9598213c63de19905
yarethsan <joogalaxy00@gmail.com>
2024-11-11 01:40:02 -0600

    se agrega query jpa para busqueda de productos

--- src/main/java/com/lexa/punto/_de/_venta/repositorios/IInventarioRepositorio.java
+++ src/main/java/com/lexa/punto/_de/_venta/repositorios/IInventarioRepositorio.java
@@ -4,6 +4,10 @@ import com.lexa.punto._de._venta.modelos.Inventario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IInventarioRepositorio extends JpaRepository<Inventario,Integer> {
+    List<Inventario> findByNombreProductoContainingOrCodigo(String nombre, String codigo);
+
 }
