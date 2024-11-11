f385e2848317c7d1a68b3bad2e12b4c0c6c15bb1
yarethsan <joogalaxy00@gmail.com>
2024-11-11 01:39:41 -0600

    se cambia el metodo de agregar detalle venta

--- src/main/java/com/lexa/punto/_de/_venta/servicios/DetalleVentaService.java
+++ src/main/java/com/lexa/punto/_de/_venta/servicios/DetalleVentaService.java
@@ -1,7 +1,13 @@
 package com.lexa.punto._de._venta.servicios;
 
 import com.lexa.punto._de._venta.modelos.DetalleVenta;
+import com.lexa.punto._de._venta.modelos.Inventario;
+import com.lexa.punto._de._venta.modelos.Venta;
 import com.lexa.punto._de._venta.repositorios.IDetalleVentaRepositorio;
+import com.lexa.punto._de._venta.repositorios.IInventarioRepositorio;
+import com.lexa.punto._de._venta.repositorios.IVentaRepositorio;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,8 +15,16 @@ import java.util.List;
 
 @Service
 public class DetalleVentaService implements IDetalleVentaService {
+
+    final static Logger logger = LoggerFactory.getLogger(DetalleVentaService.class);
     @Autowired
     private IDetalleVentaRepositorio detalleVentaRepositorio;
+
+    @Autowired
+    private IVentaRepositorio ventaRepositorio;
+
+    @Autowired
+    private IInventarioRepositorio inventarioRepositorio;
 
     @Override
     public DetalleVenta agregarDetalleVenta(DetalleVenta detalleVenta) {
@@ -23,6 +37,22 @@ public class DetalleVentaService implements IDetalleVentaService {
     @Override
     public List<DetalleVenta> agregarDetallesVenta(List<DetalleVenta> detallesVenta) {
         if (detallesVenta != null && !detallesVenta.isEmpty()) {
+            logger.info("Detalle {}", detallesVenta);
+            for (DetalleVenta detalle : detallesVenta) {
+                // Buscar la entidad Venta usando el ID
+                Venta venta = ventaRepositorio.findById(detalle.getVenta().getIdVenta())
+                        .orElseThrow(() -> new RuntimeException("La venta con ID " + detalle.getVenta().getIdVenta() + " no existe"));
+
+                // Buscar la entidad Inventario usando el ID
+                Inventario inventario = inventarioRepositorio.findById(detalle.getInventario().getIdProducto())
+                        .orElseThrow(() -> new RuntimeException("El producto con ID " + detalle.getInventario().getIdProducto() + " no existe"));
+
+                // Asignar las entidades recuperadas a DetalleVenta
+                detalle.setVenta(venta);
+                detalle.setProducto(inventario);
+            }
+
+            // Guardar todos los Detalles de Venta después de haber asignado correctamente las relaciones
             return detalleVentaRepositorio.saveAll(detallesVenta);
         }
         throw new RuntimeException("La lista de detalles de venta no puede estar vacía");
